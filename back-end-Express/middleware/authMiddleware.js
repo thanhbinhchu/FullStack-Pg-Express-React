ec2a1a41b00882b6a568526eda0885886aaa6275
thanhbinhchu <97434427+thanhbinhchu@users.noreply.github.com>
2022-06-03 11:05:36 +0200

    Update authMiddleware.js

--- back-end-Express/middleware/authMiddleware.js
+++ back-end-Express/middleware/authMiddleware.js
@@ -20,6 +20,23 @@ const validateToken = (req, res, next ) => {
 
     }
 }
+function authUser(req, res, next) {
+    const a = req.header('accessToken')
 
-module.exports = {validateToken};
+    if (a == null) {
+
+        res.status(403)
+            console.log(a)
+
+        return res.json({error : "You did not log in"})
+      
+    }else{
+        next()
+        console.log("OK") 
+        console.log(req.user)
+        console.log(a)
+    }
+  }
+
+module.exports = {validateToken, authUser};
 
